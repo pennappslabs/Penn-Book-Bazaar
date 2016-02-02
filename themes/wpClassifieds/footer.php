8b259b076f072815c4f00fc0b151c81203c19da5
Adam Domingoes <adamdom@wharton.upenn.edu>
2016-02-01 21:13:18 -0500

    Centered and clear

--- themes/wpClassifieds/footer.php
+++ themes/wpClassifieds/footer.php
@@ -9,32 +9,40 @@
  </div>
     
     
-    <div class="clear"></div>
+    <div class="clear"></div> 
 
-<div class="grid_12" id="footer">
-    <ul class="pages">
+<div id="footer" style="padding-left: 0px">
+    <ul style="" class="pages">
 	    <?php if(FRIENDLY_URL) {?>
-	 	     <li><a href="<?php echo SITE_URL;?>/content/faq.php"><?php echo _("Frequently Asked Questions");?></a></li>
-		    <li><a href="<?php echo SITE_URL."/".u(_("Privacy Policy"));?>.htm"><?php echo _("Privacy Policy");?></a></li>
-		    
-		         <li><a href="<?php echo SITE_URL;?>/content/terms.php"><?php echo _("Terms of Use");?></a></li>
-		          <li><a href="<?php echo SITE_URL;?>/content/acknowledgments.php"><?php echo _("Acknowledgments");?></a></li>
-		     
+	 	        <li><a href="<?php echo SITE_URL;?>/content/faq.php"><?php echo _("Frequently Asked Questions");?></a></li>
+                <li> | </li>
+                <li><a href="<?php echo SITE_URL."/".u(_("Privacy Policy"));?>.htm"><?php echo _("Privacy Policy");?></a></li>
+		        <li> | </li> 
+		        <li><a href="<?php echo SITE_URL;?>/content/terms.php"><?php echo _("Terms of Use");?></a></li>
+		        <li> | </li>
+                <li><a href="<?php echo SITE_URL;?>/content/acknowledgments.php"><?php echo _("Acknowledgments");?></a></li>
+		        <li> | </li> 
 	    <?php }else { ?>
-	    	     <li><a href="<?php echo SITE_URL;?>/content/faq.php"><?php echo _("Frequently Asked Questions");?></a></li>
-		         <li><a href="<?php echo SITE_URL;?>/content/terms.php"><?php echo _("Terms of Use");?></a></li>
-		          <li><a href="<?php echo SITE_URL."/".u(_("Privacy Policy"));?>.htm"><?php echo _("Privacy Policy");?></a></li>
-		          <li><a href="<?php echo SITE_URL;?>/content/acknowledgments.php"><?php echo _("Acknowledgments");?></a></li>
-	    <?php } ?>
-	        <li><a href="<?php echo SITE_URL."/".u(_("Advanced Search"));?>.htm"><?php echo _("Advanced Search");?></a></li>
-	    <li><a href="<?php echo SITE_URL."/".contactURL();?>"><?php echo _("Contact");?></a></li>
-	    <li><a href="<?php echo SITE_URL.newURL();?>"><?php echo _("Post a New Book");?></a></li>
-	         	    <li><a href="<?php echo SITE_URL."/".u(_("Sitemap"));?>.htm"><?php echo _("Sitemap");?></a></li>  
+	    	    <li><a href="<?php echo SITE_URL;?>/content/faq.php"><?php echo _("Frequently Asked Questions");?></a></li>
+                <li> | </li> 
+                <li><a href="<?php echo SITE_URL;?>/content/terms.php"><?php echo _("Terms of Use");?></a></li>
+                <li> | </li>
+                <li><a href="<?php echo SITE_URL."/".u(_("Privacy Policy"));?>.htm"><?php echo _("Privacy Policy");?></a></li>
+                <li> | </li>  
+                <li><a href="<?php echo SITE_URL;?>/content/acknowledgments.php"><?php echo _("Acknowledgments");?></a></li>
+                <li> | </li>
+        <?php } ?>
+	            <li><a href="<?php echo SITE_URL."/".u(_("Advanced Search"));?>.htm"><?php echo _("Advanced Search");?></a></li>
+                <li> | </li>
+                <li><a href="<?php echo SITE_URL."/".contactURL();?>"><?php echo _("Contact");?></a></li>
+                <li> | </li>
+                <li><a href="<?php echo SITE_URL.newURL();?>"><?php echo _("Post a New Book");?></a></li>
+                <li> | </li>
+                <li><a href="<?php echo SITE_URL."/".u(_("Sitemap"));?>.htm"><?php echo _("Sitemap");?></a></li>  
 	</ul>
     <p>
-    <?php echo SITE_NAME;?> |
 <!-- Open Classifieds License. To remove please visit http://open-classifieds.com/services/  -->
-Powered by <a title="free open source php classifieds script" href="http://www.open-classifieds.com">Open Classifieds</a>
+<div style="text-align: right; font-size: 8px"><?php echo SITE_NAME;?> | Powered by <a style="font-size: 8px" title="free open source php classifieds script" href="http://www.open-classifieds.com">Open Classifieds</a></div>
 <!--End Open Classifieds License-->
   </div>
   <div class="clear"></div>
